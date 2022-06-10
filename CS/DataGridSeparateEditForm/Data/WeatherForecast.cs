6d48823ca4c75e836845cc0de7402953acf01df1
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2022-06-10 14:47:14 +0300

    upd

--- CS/DataGridSeparateEditForm/Data/WeatherForecast.cs
+++ CS/DataGridSeparateEditForm/Data/WeatherForecast.cs
@@ -1,16 +1,9 @@
-using System;
-
-namespace DataGridSeparateEditForm.Data
-{
-    public class WeatherForecast
-    {
+namespace DataGridSeparateEditForm.Data {
+    public class WeatherForecast {
         public int ID { get; set; }
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
-
         public double TemperatureF => Math.Round((TemperatureC * 1.8 + 32), 2);
-
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
